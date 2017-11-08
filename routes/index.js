3c0dee7e6a4bb70cb0e2a06b7af75cd7cd2156b4
Chase (Ryan Cline) <ryancline88@yahoo.com>
2017-11-08 12:00:45 -0700

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -18,20 +18,22 @@ router.get('/movies', function(req, res, next) {
 });
 
 router.get('/movies/:id', function(req, res, next) {
-// write code here
-
-  // .then((movieInfo) => {
-  //   res.status(201).json({
-  //     status: 'success',
-  //     data: movieInfo
-  //   });
-  // })
-  // .catch((err) => {
-  //   res.status(500).json({
-  //     status: 'error',
-  //     data: err.message
-  //   });
-  // });
+  return dbConnection('actor_movie').where('movie_id', req.params.id)
+    .select('movie.title', 'actor.name')
+    .innerJoin('movie', 'movie.id', 'movie_id')
+    .innerJoin('actor', 'actor.id', 'actor_id')
+  .then((movieInfo) => {
+    res.status(201).json({
+      status: 'success',
+      data: movieInfo
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err.message
+    });
+  });
 });
 
 module.exports = router;
